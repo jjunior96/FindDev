f6fefd2ccb4f2fa90b9820b8f7cdd7055bfdb48f
Junior Alves <jjalves96@gmail.com>
2020-01-15 08:39:44 -0300

    install and config eslint & prettier

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-0laro.mongodb.net/week10?retryWrites=true&w=majority', { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-});
+mongoose.connect(
+  'mongodb+srv://omnistack:omnistack@cluster0-0laro.mongodb.net/week10?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(express.json());
 app.use(routes);
-
-
 
 app.listen(3333);
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const routes = Router();
 
-routes.post('/users', (req, res) => {
-  return res.json({ ok: true});
+routes.post("/users", (req, res) => {
+  return res.json({ ok: true });
 });
 
 module.exports = routes;
